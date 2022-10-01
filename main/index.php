2b25e5e3bebfc9ce48da4f2f6c43a1ed33f7cb80
felipe <85032728+Flipe124@users.noreply.github.com>
2022-10-01 10:07:42 -0300

    adicionando preset de pastas

---
+++ main/index.php
@@ -0,0 +1,11 @@
+<?php include_once("../includes/header.php"); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6"> teste </div>
+        <div class="col-md-6"> teste </div>
+    </div>
+</div>
+
+
+<?php include_once("../includes/footer.php"); ?>
