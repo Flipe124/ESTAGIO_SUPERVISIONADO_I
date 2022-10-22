e545010df9bd6005737500ead4250406d2a48dad
felipe <85032728+Flipe124@users.noreply.github.com>
2022-10-22 00:24:32 -0300

    Alterando: escopo das divs com id="card"

--- project/main/index.php
+++ project/main/index.php
@@ -12,32 +12,36 @@ $sql_expense = ("SELECT * FROM `expense` ORDER BY id DESC LIMIT 1");
 $expenses = $connection->connection()->query($sql_expense)->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-
-<div class="site-section">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="btn btn-primary col-md-10 text-center mt-2" id="balance">
-                <h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 mt-5">
+            <button class="btn btn-primary text-start" id="card">
+                <h4>
                     <label for=""><i class="bi bi-bank"></i> Saldo:</label>
+                    <br>
                     <?php foreach ($balances as $balance) {  ?>
-                        <a href="http://">R$ <?php echo $balance["balance"] ?></a>
+                        <b href="http://">R$ <?php echo $balance["balance"] ?></b>
                     <?php  } ?>
-                </h2>
-            </div>
-            <div class="btn btn-success col-md-5 text-start gain-and-expense mt-2">
-                <h3>
+                </h4>         
+            </button>
+        </div>
+        <div class="col-md-4 mt-5">
+            <button class="btn btn-success text-start" id="card">
+                <h4>
                     <label for=""><i class="bi bi-arrow-up-circle-fill"></i> Ganhos:</label>
-                </h3>
-            </div>
-            <div class="btn btn-danger col-md-5 text-start gain-and-expense mt-2">
-                <h3>
+                </h4>
+            </button>
+        </div>
+        <div class="col-md-4 mt-5">
+            <button class="btn btn-danger text-start" id="card">
+                <h4>
                     <label for=""><i class="bi bi-arrow-down-circle-fill"></i> Despesas:</label>
-                </h3>
-            </div>
-            <hr class="mt-3">
-            <div class="col-md-12 text-center mt-1">
-                <h3>Transações</h3>
-            </div>
+                </h4>
+            </button>
+        </div>
+        <hr class="mt-3">
+        <div class="col-md-12 text-center mt-1">
+            <h3>Transações</h3>
         </div>
     </div>
 </div>
