e6f27505400d73df14a12c2f271746519d4b00ac
felipe <85032728+Flipe124@users.noreply.github.com>
2022-10-21 23:11:54 -0300

    Adicionando: sidebar

---
+++ project/includes/sidebar.php
@@ -0,0 +1,25 @@
+<aside class="sidebar">
+    <div class="toggle">
+        <a href="#" class="burger js-menu-toggle" data-toggle="collapse" data-target="#main-navbar">
+            <span></span>
+        </a>
+    </div>
+    <div class="side-inner">
+        <div class="logo-wrap">
+            <div class="logo bg-dark">
+                <span><i class="fa-solid fa-chart-simple"></i></span>
+            </div>
+            <span class="logo-text">OpenFinance</span>
+        </div>
+        <div class="nav-menu">
+            <ul>
+                <li class="active"><a href="#" class="d-flex align-items-center"><span class="wrap-icon mr-3"><i class="fa-sharp fa-solid fa-house"></i></span><span class="menu-text">Dashboard</span></a></li>
+                <li><a href="#" class="d-flex align-items-center"><span class="wrap-icon icon-videocam mr-3"></span><span class="menu-text"> Videos</span></a></li>
+                <li><a href="#" class="d-flex align-items-center"><span class="wrap-icon icon-book mr-3"></span><span class="menu-text"> Books</span></a></li>
+                <li><a href="#" class="d-flex align-items-center"><span class="wrap-icon icon-shopping-cart mr-3"></span><span class="menu-text">Store</span></a></li>
+                <li><a href="#" class="d-flex align-items-center"><span class="wrap-icon icon-pie-chart mr-3"></span><span class="menu-text">Analytics</span></a></li>
+                <li><a href="#" class="d-flex align-items-center"><span class="wrap-icon icon-cog mr-3"></span><span class="menu-text">Settings</span></a></li>
+            </ul>
+        </div>
+    </div>
+</aside>
