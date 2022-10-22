53dd0ab121cee090ead0c63a83b55a045c66df45
felipe <85032728+Flipe124@users.noreply.github.com>
2022-10-21 23:13:38 -0300

    adicionando: importação dos arquivos do sidebar; adicionando: importação do dontawesome

--- project/includes/footer.php
+++ project/includes/footer.php
@@ -1,3 +1,10 @@
+<script src="../framework/06/js/jquery-3.3.1.min.js"></script>
+<script src="../framework/06/js/popper.min.js"></script>
+<script src="../framework/06/js/bootstrap.min.js"></script>
+<script src="../framework/06/js/main.js"></script>
+
+<script src="https://kit.fontawesome.com/1abad94d22.js" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
