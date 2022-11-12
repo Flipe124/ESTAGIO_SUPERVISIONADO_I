0a7d13615cc393e35186cf239dad91f2a27e95cb
Felipe Oliveira <flipeoliveira007@gmail.com>
2022-11-12 12:02:02 -0300

    Adicionando: esboço da página de despesas

--- project/frontend/registration/expense.php
+++ project/frontend/registration/expense.php
@@ -2,6 +2,19 @@
 
 <?php include_once("../includes/sidebar.php"); ?>
 
-teste despesa
+<div class="container">
+    <div class="row ms-4">
+        <div class="col-md-6 mt-4">
+            <h1>Despesas</h1>
+        </div>
+        <div class="col-md-6 mt-4 text-end">
+            <button class="mt-3 btn btn-success">+ NOVA DESPESA</button>
+        </div>
+        <div class="col-md-12">
+            <div class="line-red"></div>
+        </div>
+        <div class="flow"></div>
+    </div>
+</div>
 
 <?php include_once("../includes/footer.php"); ?>
