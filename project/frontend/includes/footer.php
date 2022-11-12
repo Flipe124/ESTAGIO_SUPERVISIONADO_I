df20263ebd6a232ebcb25efeac7c11757f46e7aa
Felipe Oliveira <flipeoliveira007@gmail.com>
2022-11-12 10:06:23 -0300

    Corrigindo: endereço dos links

--- project/frontend/includes/footer.php
+++ project/frontend/includes/footer.php
@@ -1,7 +1,7 @@
-<script src="../framework/06/js/jquery-3.3.1.min.js"></script>
-<script src="../framework/06/js/popper.min.js"></script>
-<script src="../framework/06/js/bootstrap.min.js"></script>
-<script src="../framework/06/js/main.js"></script>
+<script src="../../framework/sidebar/js/jquery-3.3.1.min.js"></script>
+<script src="../../framework/sidebar/js/popper.min.js"></script>
+<script src="../../framework/sidebar/js/bootstrap.min.js"></script>
+<script src="../../framework/sidebar/js/main.js"></script>
 
 <script src="https://kit.fontawesome.com/1abad94d22.js" crossorigin="anonymous"></script>
 
