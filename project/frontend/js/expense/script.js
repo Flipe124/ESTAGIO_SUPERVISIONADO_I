6312f09a3382d86c26497f9fb33c71d4838b7cf3
Felipe <flipeoliveira007@gmail.com>
2022-11-14 22:36:24 -0300

    Adicionando: funções para os métodos on.click

--- project/frontend/js/expense/script.js
+++ project/frontend/js/expense/script.js
@@ -1,4 +1,18 @@
+//Botão "nova despesa", abre o modal nova despesa.
 $("#btn-open-modal-expense").on("click", function () {
-    $("#modal-new-expense").modal("show")
-    console.log("clicado")
+    openModal("new-expense")
 })
+
+//Botão "fechar", fecha o modal nova despesa.
+$(".btn-close-modal-expense").on("click", function () {
+    closeModal("new-expense")
+})
+
+// Funções
+function closeModal(modalName) {
+    $("#modal-" + modalName).modal("hide")
+}
+
+function openModal(modalName) {
+    $("#modal-" + modalName).modal("show")
+}
