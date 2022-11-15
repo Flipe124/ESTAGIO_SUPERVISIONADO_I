3b87a5863cf269323fa4a4c228415a730ca7852c
Felipe <flipeoliveira007@gmail.com>
2022-11-14 21:30:40 -0300

    Alterando: link de importação; Adicionando  link do script.js de despesa

--- project/frontend/includes/footer.php
+++ project/frontend/includes/footer.php
@@ -1,7 +1,9 @@
-<script src="../../framework/sidebar/js/jquery-3.3.1.min.js"></script>
-<script src="../../framework/sidebar/js/popper.min.js"></script>
-<script src="../../framework/sidebar/js/bootstrap.min.js"></script>
-<script src="../../framework/sidebar/js/main.js"></script>
+<script src="../framework/JQuery/script.js"></script>
+<script src="../framework/sidebar/js/jquery-3.3.1.min.js"></script>
+<script src="../framework/sidebar/js/popper.min.js"></script>
+<script src="../framework/sidebar/js/bootstrap.min.js"></script>
+<script src="../framework/sidebar/js/main.js"></script>
+<script src="../js/expense/script.js"></script>
 
 <script src="https://kit.fontawesome.com/1abad94d22.js" crossorigin="anonymous"></script>
 
