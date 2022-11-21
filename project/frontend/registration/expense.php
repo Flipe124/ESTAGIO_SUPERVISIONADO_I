8bb32e213a4382fb1575bf1156b4d14988fae814
Felipe Oliveira <flipeoliveira007@gmail.com>
2022-11-21 12:00:20 -0300

    Adicionando: funções get para pegar informações em outra tabela

--- project/frontend/registration/expense.php
+++ project/frontend/registration/expense.php
@@ -8,6 +8,35 @@ $sql = ("SELECT * FROM `finance` WHERE `type` = 'EXPENSE' ");
 $expenses = $connection->connection()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<?php
+function getCategory($id)
+{
+    $connection = new Database();
+
+    $sqlCategory = ("SELECT * FROM `category` WHERE `id` = '$id' ");
+
+    $categorys = $connection->connection()->query($sqlCategory)->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($categorys as $category) {
+        echo $category['name'];
+    }
+}
+?>
+
+<?php
+function getAccount($id)
+{
+    $connection = new Database();
+
+    $sqlAccount = ("SELECT * FROM `account` WHERE `id` = '$id' ");
+
+    $Accounts = $connection->connection()->query($sqlAccount)->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($Accounts as $Account) {
+        echo $Account['name'];
+    }
+}
+?>
 
 <div class="container">
     <!-- MODAL NOVA DESPESA -->
@@ -69,46 +98,49 @@ $expenses = $connection->connection()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
             <div class="p-3 text-dark" id="expense-total">
                 <h4>Total de despesas:</h4>
                 <h5 class="text-danger"><b>R$ - 1.788,98</b></h5>
-            </div>    
+            </div>
         </div>
         <div class="col-md-12 mt-3 text-center">
-            <div class="btn btn-danger"><</div>
-            <div class="btn btn-danger"><b>Novembro</b> 2022</div>
-            <div class="btn btn-danger">></div>
+            <div class="btn btn-danger">
+                <div class="btn btn-danger"><</div>
+                <div class="btn btn-danger"><b>Novembro</b> 2022</div>
+                <div class="btn btn-danger">></div>
+            </div>
         </div>
-        <div class="col-md-12 mt-3">
-            <div id="div-table">
-                <table class="table table-light table-striped">
-                    <thead>
-                        <tr>
-                            <th>Valor</th>
-                            <th>Data</th>
-                            <th>Descrição</th>
-                            <th>Categoria</th>
-                            <th>Conta</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($expenses as $expense) { ?>
-                        <tr>
-                            <th class="text-danger"><?php echo "R$ - " . $expense['value']?></th>
-                            <th><?php echo $expense['date']?></th>
-                            <th><?php echo $expense['description']?></th>
-                            <th><?php echo $expense['category_id']?></th>
-                            <th><?php echo $expense['account_id']?></th>
-                            <th>
-                                <button class="btn btn-danger" type="button"><i class="fa-solid fa-trash"></i></button>
-                                <button class="btn btn-primary" type="button"><i class="fa-solid fa-pen"></i></button>
-                            </th>
-                        </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+            <div class="col-md-12 mt-3">
+                <div id="div-table">
+                    <table class="table table-light table-striped">
+                        <thead>
+                            <tr>
+                                <th>Valor</th>
+                                <th>Data</th>
+                                <th>Descrição</th>
+                                <th>Categoria</th>
+                                <th>Conta</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($expenses as $expense) { ?>
+                                <tr>
+                                    <th class="text-danger"><?php echo "R$ - " . $expense['value'] ?></th>
+                                    <th><?php echo date('d/m/Y', strtotime($expense['date'])); ?></th>
+                                    <th><?php echo $expense['description'] ?></th>
+                                    <th><?php echo getCategory($expense['category_id']) ?></th>
+                                    <th><?php echo getAccount($expense['account_id']) ?></th>
+                                    <th>
+                                        <button class="btn btn-danger" type="button"><i class="fa-solid fa-trash"></i></button>
+                                        <button class="btn btn-primary" type="button"><i class="fa-solid fa-pen"></i></button>
+                                    </th>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
         <div class="flow"></div>
     </div>
-</div>
+</div>    
 
 <?php include_once("../includes/footer.php"); ?>
