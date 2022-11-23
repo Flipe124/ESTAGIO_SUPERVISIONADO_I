fdafd40ecd3fce6689e6c5e279a684cdef004fab
Felipe <flipeoliveira007@gmail.com>
2022-11-23 10:21:25 -0300

    Adicionando: importação da biblioteca Select2

--- project/frontend/includes/footer.php
+++ project/frontend/includes/footer.php
@@ -4,6 +4,7 @@
 <script src="../framework/sidebar/js/bootstrap.min.js"></script>
 <script src="../framework/sidebar/js/main.js"></script>
 <script src="../js/expense/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <script src="https://kit.fontawesome.com/1abad94d22.js" crossorigin="anonymous"></script>
 
--- project/frontend/includes/header.php
+++ project/frontend/includes/header.php
@@ -12,7 +12,6 @@
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
-
   <!-- SIDEBAR -->
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
@@ -21,20 +20,19 @@
   <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&display=swap" rel="stylesheet">
 
   <!-- <link rel="stylesheet" href="fonts/icomoon/style.css"> -->
-  <!-- <link rel="stylesheet" href="../framework/06/fonts/icomoon/style.css"> -->
   <link rel="stylesheet" href="../framework/sidebar/fonts/icomoon/style.css">
 
   <!-- <link rel="stylesheet" href="css/owl.carousel.min.css"> -->
-  <!-- <link rel="stylesheet" href="../framework/06/css/owl.carousel.min.css"> -->
   <link rel="stylesheet" href="../framework/sidebar/css/owl.carousel.min.css">
 
   <!-- Bootstrap CSS -->
-  <!-- <link rel="stylesheet" href="../framework/06/css/bootstrap.min.css"> -->
   <link rel="stylesheet" href="../framework/sidebar/css/bootstrap.min.css">
 
   <!-- Style -->
-  <!-- <link rel="stylesheet" href="../framework/06/css/style.css"> -->
   <link rel="stylesheet" href="../framework/sidebar/css/style.css">
+
+  <!-- Select2 -->
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
 
 
