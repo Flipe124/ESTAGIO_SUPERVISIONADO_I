1eef3eb3a0bb6a8712ca285e5624c2a0f4f96f47
Felipe Oliveira <flipeoliveira007@gmail.com>
2022-11-26 20:33:49 -0300

    Adicionando: consulta no banco de dados

--- project/frontend/main/index.php
+++ project/frontend/main/index.php
@@ -3,13 +3,20 @@
 <?php include_once("../includes/sidebar.php"); ?>
 
 <?php
-// $sql_balance = ("SELECT * FROM `balance` ORDER BY id DESC LIMIT 1");
+//  $sql_balance = ("SELECT * FROM `balance` ORDER BY id DESC LIMIT 1");
 
 // $balances = $connection->connection()->query($sql_balance)->fetchAll(PDO::FETCH_ASSOC);
 
 // $sql_expense = ("SELECT * FROM `expense` ORDER BY id DESC LIMIT 1");
 
 // $expenses = $connection->connection()->query($sql_expense)->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<?php
+$sql_balance = ("SELECT SUM(`balance`) FROM `account`");
+
+$balances = $connection->connection()->query($sql_balance)->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <div class="container">
@@ -22,9 +29,11 @@
                 <h4>
                     <label for=""><i class="bi bi-bank"></i> Saldo:</label>
                     <br>
-                    <!-- <?php foreach ($balances as $balance) {  ?>
-                        <b href="http://">R$ <?php echo $balance["balance"] ?></b>
-                    <?php  } ?> -->
+                    <?php foreach ($balances as $balance) {  
+                        print_r($balance)
+                        ?>
+                        <b>R$ <?php echo number_format($balance['SUM(balance)'],2,",",".") ?></b>
+                    <?php  } ?>
                 </h4>
             </button>
         </div>
@@ -36,11 +45,11 @@
             </button>
         </div>
         <div class="col-md-4 mt-3">
-            <button class="btn btn-danger text-start" id="card">
+            <a class="btn btn-danger" href="../registration/expense.php" id="card">
                 <h4>
-                    <label for=""><i class="bi bi-arrow-down-circle-fill"></i> Despesa:</label>
+                    <label for=""><i class="bi bi-arrow-down-circle-fill"></i> Despesa:  <?php echo date('m/Y')?></label>
                 </h4>
-            </button>
+            </a>
         </div>
         <hr class="mt-3">
         <div class="col-md-12 text-center mt-1">
