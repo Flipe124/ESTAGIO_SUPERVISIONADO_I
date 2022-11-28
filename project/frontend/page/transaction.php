211753f3888cd14db436865cd06e9cb01da18b82
Felipe <flipeoliveira007@gmail.com>
2022-11-27 22:02:44 -0300

    Adicionando: tabela de consulta de finanças

---
+++ project/frontend/page/transaction.php
@@ -0,0 +1,59 @@
+<?php include_once("../includes/header.php"); ?>
+
+<?php include_once("../includes/sidebar.php"); ?>
+
+<?php
+$sql_finance = ("SELECT * FROM `finance` ORDER BY `date` DESC");
+
+$finances = $connection->connection()->query($sql_finance)->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<div class="container">
+    <div class="row ms-4">
+        <div class="col-md-12 mt-4">
+            <h1>Transações</h1>
+        </div>
+        <div class="col-md-12 mt-3">
+            <div class="text-center" id="div-table">
+                <table class="table table-light table-striped">
+                    <thead>
+                        <tr>
+                            <th>Situação</th>
+                            <th>Valor</th>
+                            <th>Data</th>
+                            <th>Descrição</th>
+                            <th>Categoria</th>
+                            <th>Conta</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($finances as $finance) { ?>
+                            <tr>
+                                <?php if ($finance["status"] == "PAID") { ?>
+                                    <th><i class="text-success fa-solid fa-circle-check"></i></th>
+                                <?php } else { ?>
+                                    <th><i class="text-danger fa-solid fa-circle-xmark"></i></th>
+                                <?php } ?>
+                                <?php if ($finance["type"] == "EXPENSE") { ?>
+                                    <td class="text-danger"><?php echo "R$ - " . number_format($finance['value'], 2, ",", "."); ?></td>
+                                <?php } else { ?>
+                                    <td class="text-success"><?php echo "R$ " . number_format($finance['value'], 2, ",", "."); ?></td>
+                                <?php } ?>
+                                <td><?php echo date('d/m/Y', strtotime($finance['date'])); ?></td>
+                                <td><?php echo $finance["description"]; ?></td>
+                                <td><?php echo getCategory($finance["category_id"]); ?></td>
+                                <td><?php echo getAccount($finance["account_id"]); ?></td>
+                                <td>
+                                    <button class="btn btn-danger btn-delete-expense" type="button"><i class="fa-solid fa-trash"></i></button>
+                                    <button class="btn btn-primary btn-edit-expense" type="button"><i class="fa-solid fa-pen"></i></button>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once("../includes/footer.php"); ?>
