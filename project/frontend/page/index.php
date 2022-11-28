6419c366d756931ddbb781741ce32c1deba44d85
Felipe <flipeoliveira007@gmail.com>
2022-11-27 22:03:13 -0300

    Retirando: tabela de finanças

--- project/frontend/page/index.php
+++ project/frontend/page/index.php
@@ -57,44 +57,10 @@ $balances = $connection->connection()->query($sql_balance)->fetchAll(PDO::FETCH_
         </div>
         <hr class="mt-3">
         <div class="col-md-12 text-center mt-1">
-            <h3>Transações</h3>
+            <h3>Pendente</h3>
         </div>
         <div class="col-md-12">
-            <div class="text-center" id="div-table">
-                <table class="table table-light table-striped">
-                    <thead>
-                        <tr>
-                            <th>Situação</th>
-                            <th>Valor</th>
-                            <th>Data</th>
-                            <th>Descrição</th>
-                            <th>Categoria</th>
-                            <th>Conta</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($finances as $finance) { ?>
-                            <tr>
-                                <th><?php echo $finance["status"]; ?></th>
-                                <?php if($finance["type"] == "EXPENSE") { ?>
-                                    <td class="text-danger"><?php echo "R$ - ". number_format($finance['value'],2,",","."); ?></td>
-                                <?php } else { ?>
-                                    <td class="text-success"><?php echo "R$ ". number_format($finance['value'],2,",","."); ?></td>
-                                <?php } ?> 
-                                <td><?php echo date('d/m/Y', strtotime($finance['date'])); ?></td>
-                                <td><?php echo $finance["description"]; ?></td>
-                                <td><?php echo getCategory($finance["category_id"]); ?></td>
-                                <td><?php echo getAccount($finance["account_id"]); ?></td>
-                                <td>
-                                    <button class="btn btn-danger btn-delete-expense" type="button"><i class="fa-solid fa-trash"></i></button>
-                                    <button class="btn btn-primary btn-edit-expense" type="button"><i class="fa-solid fa-pen"></i></button>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
+            
         </div>
         <div class="col-md-12 flow">
         </div>
