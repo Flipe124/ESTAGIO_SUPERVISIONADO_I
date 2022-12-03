f7bf3b7042e192d0ff0111576db70f858d7912f7
rhuan-pk <rhuan.pksf@gmail.com>
2022-12-03 19:56:02 -0300

    atualizado: project/backend/golang/api/handles.go: listApi: implementando.

--- project/backend/golang/api/handles.go
+++ project/backend/golang/api/handles.go
@@ -2,19 +2,46 @@ package api
 
 import (
 	"encoding/json"
-	"golang/database"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func listApi(writer http.ResponseWriter, request *http.Request) {
 
-	log.Println("listApi contact!")
+	if request.Method != "GET" {
+		httpErrorMessage := http.StatusText(http.StatusMethodNotAllowed)
+		http.Error(writer, httpErrorMessage, http.StatusMethodNotAllowed)
+		log.Println("endpoint \"/list\" error:", httpErrorMessage + "!")
+		return
+	}
+
+	log.Println("endpoint \"/list\" contact!")
 
 	writer.Header().Set("Content=Type", "application/json")
-	response := json.NewEncoder(writer)
+	// response := json.NewEncoder(writer)
 
-	table, _ := database.List("table")
-	response.Encode(table)
+	// table, _ := database.List("table")
+	// response.Encode("table")
+
+	requestBody, err := io.ReadAll(request.Body)
+	if err != nil {
+		httpErrorMessage := http.StatusText(http.StatusBadRequest)
+		http.Error(writer, httpErrorMessage, http.StatusBadRequest)
+		log.Println("endpoint \"/list\" error:", httpErrorMessage + "!")
+		return
+	}
+
+	var response *any
+	err = json.Unmarshal(requestBody, &response)
+	if err != nil {
+		httpErrorMessage := http.StatusText(http.StatusInternalServerError)
+		http.Error(writer, httpErrorMessage, http.StatusInternalServerError)
+		log.Println("endpoint \"/list\" error:", httpErrorMessage + "!")
+		return
+	}
+
+	fmt.Println(*response)
 
 }
