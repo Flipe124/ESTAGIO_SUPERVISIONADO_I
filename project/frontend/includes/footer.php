f1fdd5216ea137bb9796e76036254b3c62e3b15b
Felipe <flipeoliveira007@gmail.com>
2022-12-06 22:28:57 -0300

    Alterando: link do CDN do jquery

--- project/frontend/includes/footer.php
+++ project/frontend/includes/footer.php
@@ -5,7 +5,7 @@
 <script src="../framework/sidebar/js/main.js"></script>
 <script src="../js/expense/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> -->
 
 <script src="https://kit.fontawesome.com/1abad94d22.js" crossorigin="anonymous"></script>
 
