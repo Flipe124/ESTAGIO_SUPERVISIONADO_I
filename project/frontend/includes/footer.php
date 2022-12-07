8a025c80d90bd0c8a31a980579a8a8f00e5f314b
Felipe <flipeoliveira007@gmail.com>
2022-12-07 20:51:35 -0300

    Update footer.php

--- project/frontend/includes/footer.php
+++ project/frontend/includes/footer.php
@@ -4,6 +4,7 @@
 <script src="../framework/sidebar/js/bootstrap.min.js"></script>
 <script src="../framework/sidebar/js/main.js"></script>
 <script src="../js/expense/script.js"></script>
+<script src="../js/revenue/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <!-- <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> -->
 
