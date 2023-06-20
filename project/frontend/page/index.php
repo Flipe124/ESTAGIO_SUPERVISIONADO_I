afe8222867d41f40e343b62e1158f340fe9ab71b
Felipe <flipeoliveira007@gmail.com>
2023-06-20 00:37:10 -0300

    Adicionando elementos para dashboard

--- project/frontend/page/index.php
+++ project/frontend/page/index.php
@@ -1,12 +1,51 @@
-<!-- <link href="../includes/css/service/style.css" rel="stylesheet" />
-<link href="../includes/css/address/style.css" rel="stylesheet" /> -->
+<link href="../css/dashboard/style.css" rel="stylesheet" />
+<!--<link href="../includes/css/address/style.css" rel="stylesheet" /> -->
 <!-- <link href="../includes/img/bubbles.png" rel="icon" type="image/x-icon" /> -->
+<link rel="icon" type="image/x-icon" href="../img/dollar.png" />
 <?php include_once("../includes/header.php") ?>
 </head>
 
 <body>
     <?php include_once("../includes/sidebar.php") ?>
-    <div>TESTEEEEE</div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="dashboard">
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="revenue">
+                        <b class="text-light">RECEITA:</b>
+                        <div class="text-success">
+                            R$ 1.500,00
+                        </div>
+                    </div>
+                    <div class="expense">
+                        <b class="text-light">DESPESA:</b>
+                        <div class="text-danger">
+                            R$ 1.500,00
+                        </div>
+                    </div>
+
+                    <div>TESTE2</div>
+                </div>
+                <div class="col-md-6">
+                    <div><canvas id="grafico"></canvas></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                teste
+            </div>
+        </div>
+    </div>
+
+    <div></div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="../js/graph/script.js"></script>
 
     <?php include_once("../includes/footer.php") ?>
 </body>
