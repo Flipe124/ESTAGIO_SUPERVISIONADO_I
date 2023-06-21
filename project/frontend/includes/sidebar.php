080a916b7cce433b579faab1fe7700a5f9107b67
Felipe <flipeoliveira007@gmail.com>
2023-06-20 23:20:18 -0300

    Alterando Títulos e links

--- project/frontend/includes/sidebar.php
+++ project/frontend/includes/sidebar.php
@@ -1,24 +1,27 @@
 <div class="d-flex" id="wrapper">
-    <div class="backgroud-black-blue" id="sidebar-wrapper">
+    <div class="background-white" id="sidebar-wrapper">
         <div class="sidebar-heading text-center">
             <a href="../page/index.php">
-                <img id="logotype-sidebar" src="../img/openfinance.png" width="200px" alt="logotipo">
+                <img id="logotype-sidebar" src="../img/openfinance_white.png" width="200px" alt="logotipo">
             </a>
         </div>
         <ul id="ul-sidebar-link" class="list-group list-group-flush">
             <li class="option-not-selected">
-                <a class="list-group-item p-3 option-not-selected" href="../page/index.php"><i class="fas fa-tools"></i> <b>DASHBOARD</b></a>
+                <a class="list-group-item p-3 option-not-selected" href="../page/index.php"><i class="fas fa-chart-line fa-lg"></i> <b>DASHBOARD</b></a>
             </li>
             <li>
-                <a class="list-group-item p-3 option-not-selected" href="../page/client.php"><i class="fa-sharp fa-solid fa-user-tie"></i> <b>RECEITAS</b></a>
+                <a class="list-group-item p-3 option-not-selected" href="../page/revenue.php"><i class="fas fa-chevron-circle-up fa-lg"></i> <b>RECEITAS</b></a>
             </li>
             <li>
-                <a class="list-group-item p-3 option-not-selected" href="../page/user.php"><i class="fa-solid fa-user"></i> <b>DESPESAS</b></a>
+                <a class="list-group-item p-3 option-not-selected" href="../page/expense.php"><i class="fas fa-chevron-circle-down fa-lg"></i> <b>DESPESAS</b></a>
+            </li>
+            <li>
+                <a class="list-group-item p-3 option-not-selected" href="../page/report.php"><i class="far fa-file-alt fa-lg"></i> <b>RELATÓRIOS</b></a>
             </li>
         </ul>
     </div>
     <div id="page-content-wrapper">
-        <nav class="navbar backgroud-black-blue">
+        <nav class="navbar background-white">
             <div class="container-fluid">
                 <span>
                     <button class="fs-2 ms-1" id="sidebarToggle">
