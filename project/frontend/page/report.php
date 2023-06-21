14cbfc7d5aebfca04359efb1448cbae7e80ac3b6
Felipe <flipeoliveira007@gmail.com>
2023-06-20 23:11:17 -0300

    Adicionando tela de report/relatórios

---
+++ project/frontend/page/report.php
@@ -0,0 +1,18 @@
+<!-- <link href="../css/dashboard/style.css" rel="stylesheet" /> -->
+<link rel="icon" type="image/x-icon" href="../img/dollar.png" />
+<?php include_once("../includes/header.php") ?>
+</head>
+
+<body>
+    <?php include_once("../includes/sidebar.php") ?>
+    <div class="container-fluid">
+        <div class="row">
+        </div>
+    </div>
+
+    <div></div>
+    <?php include_once("../includes/footer.php") ?>
+</body>
+
+
+</html>
