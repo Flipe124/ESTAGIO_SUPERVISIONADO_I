b7abcc4fb1e21f1873c845a1993231bee93cc766
Felipe <flipeoliveira007@gmail.com>
2023-06-24 23:37:15 -0300

    Adicionando estruturas para a tela de dashboard

--- project/frontend/page/index.php
+++ project/frontend/page/index.php
@@ -7,6 +7,33 @@
     <?php include_once("../includes/sidebar.php") ?>
     <div class="container-fluid">
         <div class="row">
+            <div class="col-md-12 fs-2"><b>Dashboard</b></div>
+            <div class="col-md-12 text-center text-light">
+                <div class="box-dashboard mt-2 bg-primary">
+                    <div class="pt-2 ps-2 text-start">
+                    <i class="fa-solid fa-building-columns"></i> Saldo
+                    </div>
+                    <div class="pt-2 ps-2 fs-6 text-start">
+                        <b>R$ 1.600,55</b>
+                    </div>
+                </div>
+                <div class="box-dashboard mt-2 bg-success">
+                    <div class="pt-2 ps-2 text-start">
+                    <i class="fas fa-chevron-circle-up"></i> Receitas
+                    </div>
+                    <div class="pt-2 ps-2 fs-6 text-start">
+                        <b>R$ 1.600,55</b>
+                    </div>
+                </div>
+                <div class="box-dashboard mt-2 bg-danger">
+                    <div class="pt-2 ps-2 text-start">
+                    <i class="fas fa-chevron-circle-down"></i> Despesas
+                    </div>
+                    <div class="pt-2 ps-2 fs-6 text-start">
+                        <b>R$ 101.600,55</b>
+                    </div>
+                </div>
+            </div>
             <div class="col-md-12">
                 <div class="dashboard">
                     <div>
