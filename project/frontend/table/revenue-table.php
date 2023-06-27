3da4665bc4b2956c95f8ce5eafa944921af6a612
Felipe <flipeoliveira007@gmail.com>
2023-06-26 23:58:54 -0300

    Adicionando tabela estática

---
+++ project/frontend/table/revenue-table.php
@@ -0,0 +1,38 @@
+<div class="revenue-table results">
+    <div class="result">
+        <span class="icon-category">
+            <i class="fab fa-sellcast text-success"></i>
+        </span>
+        <span class="description">
+            <span class="category"> <b>Salário</b></span>
+            <h6 class="data">
+                QuantumTech | Nubank
+            </h6>
+        </span>
+        <div class="value text-success">
+            <b class="text-value">R$ 12.300,30</b> <span class="status mb-1 ms-1"><i class="fas fa-check-circle text-danger"></i></span>
+        </div>
+        <span class="buttons">
+            <button class="btn btn-outline-danger button-delete-operation" type="button" data-id="1" data-category="um" data-valueDel="12300.30"><i class="fa-solid fa-trash"></i></button>
+            <button class="btn btn-primary button-update-operation" type="button" type="button" data-id="1" data-category="um" data-value="12300.30"><i class="fa-solid fa-pen"></i></button>
+        </span>
+    </div>
+    <div class="result">
+        <span class="icon-category">
+            <i class="fas fa-coins text-warning"></i>
+        </span>
+        <span class="description">
+            <span class="category"><b>Venda</b></span>
+            <h6 class="data">
+                Mercado Livre | Nubank
+            </h6>
+        </span>
+        <div class="value text-success">
+            <b>R$ 10.000,00</b> <span class="status mb-1 ms-1"><i class="fas fa-check-circle text-danger"></i></span>
+        </div>
+        <span class="buttons">
+            <button class="btn btn-outline-danger button-delete-operation" type="button" data-id="2" data-category="dois" data-valueDel="10000.00"><i class="fa-solid fa-trash"></i></button>
+            <button class="btn btn-primary button-update-operation" type="button" data-id="2" data-category="dois" data-value="10000.00"><i class="fa-solid fa-pen"></i></button>
+        </span>
+    </div>
+</div>
