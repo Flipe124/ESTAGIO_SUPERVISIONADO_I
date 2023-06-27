6888f448c4d55ca09928828a4909b2f27244573c
Felipe <flipeoliveira007@gmail.com>
2023-06-26 23:57:54 -0300

    Adicionando novos modais: new , update e delete

---
+++ project/frontend/modal/modal-delete.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="modal-delete" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-danger text-light">
+                <h5 class="modal-title"><b>EXCLUIR</b></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="post" id="form-delete">
+                    <input type="hidden" name="delete_id" id="delete-id">
+                    <div class="row">
+                    </div>
+                    <div class="modal-footer mt-3" id="footer-modal-delete">
+                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">CANCELAR</button>
+                        <button type="button" class="btn btn-success" id="button-confirm-delete">SIM</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ project/frontend/modal/modal-new.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="modal-create" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-success text-light">
+                <h5 class="modal-title"><b>NOVA</b></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post" name="form_create" id="form-create">
+                    <input type="hidden" name="create_id" id="create-id">
+                    <div class="row">
+                        <div class="col-md-12 mt-2">
+                            <label class="form-label text-danger">Campos obrigatório *</label>
+                        </div>
+                    </div>
+                    <div class="modal-footer mt-3" id="footer-modal-create">
+                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">CANCELAR</button>
+                        <button type="button" class="btn btn-success" id="button-create">SALVAR</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
