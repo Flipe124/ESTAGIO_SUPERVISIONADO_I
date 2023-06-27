19cf54a51e1bfb1d66ec4bca6612feea00b641f0
Felipe <flipeoliveira007@gmail.com>
2023-06-26 23:59:42 -0300

    teste

---
+++ project/frontend/old/testes.php
@@ -0,0 +1,13 @@
+<div class="user active">
+    <span class="user-avatar mt-1">
+        <img src="../includes/img/avatar-standard.avif">
+    </span>
+    <span class="user-description">
+        <span class="user-email">ronaldinha@gmail.com</span>
+        <h6 class="user-name">ronaldinha cunha</h6>
+    </span>
+    <span class="user-buttons">
+        <button class="btn btn-danger button-delete-user" type="button" data-id="1" data-name="ronaldinha cunha" data-username="ronaldinha" data-email="ronaldinha@gmail.com"><i class="fa-solid fa-trash"></i></button>
+        <button class="btn btn-primary button-update-user" type="button" data-id="1" data-name="ronaldinha cunha" data-username="ronaldinha" data-email="ronaldinha@gmail.com"><i class="fa-solid fa-pen"></i></button>
+    </span>
+</div>
