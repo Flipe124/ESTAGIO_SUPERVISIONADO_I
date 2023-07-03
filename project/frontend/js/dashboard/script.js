29b67acee88865c630dc4ce58173573f60d49ceb
Felipe <flipeoliveira007@gmail.com>
2023-07-02 22:41:43 -0300

    FEAT: adicionando location replace para as respectivas telas

---
+++ project/frontend/js/dashboard/script.js
@@ -0,0 +1,11 @@
+$('#box-dashboard-balance').on('click', function () {
+    location.replace('./account.php')
+});
+
+$('#box-dashboard-revenue').on('click', function () {
+    location.replace('./revenue.php')
+});
+
+$('#box-dashboard-expense').on('click', function () {
+    location.replace('./expense.php')
+});
