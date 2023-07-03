0a58849641129d27074ddbef9e8de13e60f3cd40
Felipe <flipeoliveira007@gmail.com>
2023-07-02 22:47:46 -0300

    FEAT: adicionando funções de criação de gráfico de pizza e de barras

--- project/frontend/js/graph/script.js
+++ project/frontend/js/graph/script.js
@@ -1,24 +1,57 @@
 
-// Obtém a referência do elemento canvas no HTML
-var ctx = document.getElementById('grafico').getContext('2d');
+createGraphPie();
+createGraphBar();
 
-// Dados para o gráfico de pizza
-var data = {
-    labels: ['Receita', 'Despesa'], // Rótulos das fatias
-    datasets: [{
-        data: [333, 333], // Valores das fatias
-        backgroundColor: ['green', 'red'], // Cores das fatias
-    }]
-};
+function createGraphPie() {
+    var ctx = document.getElementById('graph-pie').getContext('2d');
 
-// Configurações do gráfico
-var options = {
-    responsive: true
-};
+    var data = {
+        labels: ['Receita', 'Despesa'],
+        datasets: [{
+            data: [3780.59, 975],
+            backgroundColor: ['#198754', '#DC3547'],
+        }]
+    };
 
-// Cria o gráfico de pizza
-var chart = new Chart(ctx, {
-    type: 'pie',
-    data: data,
-    options: options
-});
+    var options = {
+        responsive: true,
+        legend: {
+            fontSize: 30
+        }
+    };
+
+    var chart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    });
+}
+
+function createGraphBar() {
+    var ctx = document.getElementById('graph-bar').getContext('2d');
+
+    var data = {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+        datasets: [{
+            label: 'Evolução do saldo',
+            data: [1200.00, 1880.00, 1600.00, 2200.00, 2300.50, 2750.20],
+            backgroundColor: '#0D6EFD'
+        }]
+    };
+
+    var options = {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+}
+
