3fbfa751c89b60c2d054d769715a11ed0df9dd7a
Felipe <flipeoliveira007@gmail.com>
2023-07-02 22:48:18 -0300

    ADD: adicionando novos elementos

--- project/frontend/page/index.php
+++ project/frontend/page/index.php
@@ -12,17 +12,16 @@
                     <div class="fs-3"><b>Dashboard</b></div>
                 </div>
             </div>
-            <!-- <div class="col-md-12 fs-2 mt-2"><b>Dashboard</b></div> -->
             <div class="col-md-12 text-center text-light">
-                <div class="box-dashboard mt-1 bg-primary">
+                <div class="box-dashboard mt-1 bg-primary filter-preset-1" id="box-dashboard-balance">
                     <div class="pt-2 ps-2 text-start">
                         <i class="fa-solid fa-building-columns"></i> Saldo
                     </div>
                     <div class="pt-2 ps-2 fs-6 text-start">
-                        <b>R$ 1.600,55</b>
+                        <b>R$ 2.300,50</b>
                     </div>
                 </div>
-                <div class="box-dashboard mt-2 bg-success">
+                <div class="box-dashboard mt-1 bg-success filter-preset-1" id="box-dashboard-revenue">
                     <div class="pt-2 ps-2 text-start">
                         <i class="fas fa-chevron-circle-up"></i> Receitas
                     </div>
@@ -30,7 +29,7 @@
                         <b>R$ 1.600,55</b>
                     </div>
                 </div>
-                <div class="box-dashboard mt-2 bg-danger">
+                <div class="box-dashboard mt-1 bg-danger filter-preset-1" id="box-dashboard-expense">
                     <div class="pt-2 ps-2 text-start">
                         <i class="fas fa-chevron-circle-down"></i> Despesas
                     </div>
@@ -40,44 +39,57 @@
                 </div>
             </div>
             <div class="col-md-12">
-                <div class="dashboard">
-                    <div>
-
+                <div class="mt-2 text-center">
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-light"><i class="fas fa-arrow-left"></i></button>
+                        <span class="date btn btn-light">Agosto</span>
+                        <button type="button" class="btn btn-light"><i class="fas fa-arrow-right"></i></button>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="revenue">
-                        <b class="text-light">RECEITA:</b>
-                        <div class="text-success">
-                            R$ 1.500,00
+            <div class="col-md-6 mt-2">
+                <div class="dashboard dashboard-preset-1 filter-preset-1">
+                    <div class="values">
+                        <div class="revenue">
+                            <span class="fs-6">Receita:
+                                <span class="text-success">
+                                    <b class="amount">R$ 3.780,59</b>
+                                </span>
+                            </span>
+                        </div>
+                        <div class="expense">
+                            <span class="">Despesa:
+                                <span class="text-danger">
+                                    <b class="amount">R$ 975,00</b>
+                                </span>
+                            </span>
                         </div>
                     </div>
-                    <div class="expense">
-                        <b class="text-light">DESPESA:</b>
-                        <div class="text-danger">
-                            R$ 1.500,00
-                        </div>
+                    <div class="text-end">
+                        <canvas id="graph-pie"></canvas>
                     </div>
-
-                    <div>TESTE2</div>
-                </div>
-                <div class="col-md-6">
-                    <div><canvas id="grafico"></canvas></div>
                 </div>
             </div>
-            <div class="col-md-6">
-                teste
+            <div class="col-md-6 mt-2">
+                <div class="dashboard dashboard-preset-1 filter-preset-1">
+                    <canvas id="graph-bar"></canvas>
+                </div>
+            </div>
+            <div class="col-md-12 mt-2">
+                <?php include_once("../table/account-table.php") ?>
             </div>
         </div>
     </div>
+    </div>
+    </div>
 
     <div></div>
+    <?php include_once("../includes/footer.php") ?>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="../js/graph/script.js"></script>
+    <script src="../js/dashboard/script.js"></script>
 
-    <?php include_once("../includes/footer.php") ?>
 </body>
 
 
