c029e78a1eba72d954f0a7de432baab342ad4c88
Felipe <flipeoliveira007@gmail.com>
2023-07-04 00:39:59 -0300

    ADD: Adicionando tabela de listagem de contas para dashboard

---
+++ project/frontend/table/account-balance-table.php
@@ -0,0 +1,42 @@
+<div id="div-table">
+    <table class="table table-striped table-borderless" id="table-account-balance">
+        <thead>
+            <tr>
+                <th class="order-by text-center" onclick="ordenarTabela(0)">
+                    Conta
+                    <i class="fas fa-sort ml-1"></i>
+                </th>
+                <th class="order-by text-center" onclick="ordenarTabela(1)">
+                    Balanço
+                    <i class="fas fa-sort ml-1"></i>
+                </th>
+                <th class="order-by text-center" onclick="ordenarTabela(2)">
+                    Saldo
+                    <i class="fas fa-sort ml-1"></i>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="result-table-account text-center">
+                <td>Nubank</td>
+                <td class="text-success">R$ 100,00</td>
+                <td>R$ 4.500,00</td>
+            </tr>
+            <tr class="result-table-account text-center">
+                <td>Inter</td>
+                <td class="text-success">R$ 50,00</td>
+                <td>R$ 2.300,00</td>
+            </tr>
+            <tr class="result-table-account text-center">
+                <td>Carteira</td>
+                <td class="text-danger">R$ 40,00</td>
+                <td>R$ 2.500,00</td>
+            </tr>
+            <tr class="result-table-account text-center">
+                <td>Carteira</td>
+                <td class="text-danger">R$ 100,00</td>
+                <td>R$ 5.500,00</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
