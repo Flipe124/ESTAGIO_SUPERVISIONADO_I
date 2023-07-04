46b2c96c620b0f277a96d4bdbcb422dbd5b40f49
Felipe <flipeoliveira007@gmail.com>
2023-07-04 20:42:18 -0300

    ADD: modal de despesa

---
+++ project/frontend/modal/modal-update-expense.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="modal-update" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-light">
+                <h5 class="modal-title"><b>EDITAR</b></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post" name="form_update" id="form-update">
+                    <input type="hidden" name="update_id" id="update-id">
+                    <div class="row">
+                        <div class="col-md-12 mt-2">
+                            <label class="form-label text-danger">Campos obrigatório *</label>
+                        </div>
+                    </div>
+                    <div class="modal-footer mt-3" id="footer-modal-update">
+                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">CANCELAR</button>
+                        <button type="button" class="btn btn-success" id="button-update-user">SALVAR</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
