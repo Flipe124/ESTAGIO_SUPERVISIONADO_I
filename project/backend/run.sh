c211021a5c326df0fc70e35dde33719848d23ddf
rhuan-pk <rhuan.pksf@gmail.com>
2023-07-05 19:07:24 -0300

    refresh

--- project/backend/run.sh
+++ project/backend/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swag fmt && swag init -g ./cmd/benlimp.go -o ./pkg/docs/ && go run -ldflags '-s -w' ./cmd/
+swag fmt && swag init -g ./cmd/openfinance.go -o ./pkg/docs/ && go run -ldflags '-s -w' ./cmd/
