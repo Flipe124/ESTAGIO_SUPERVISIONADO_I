1920c4c64dbe8e9e12df49fcf7c3e25a486f847a
Felipe <flipeoliveira007@gmail.com>
2023-07-05 22:17:53 -0300

    MOD: adicionando valor de checkbox e valores de option estáticos

--- project/frontend/modal/modal-update.php
+++ project/frontend/modal/modal-update.php
@@ -8,6 +8,7 @@
             <div class="modal-body">
                 <form action="" method="post" name="form_update" id="form-update">
                     <input type="hidden" name="update_id" id="update-id">
+                    <input type="hidden" id="update-input-type-operation">
                     <div class="row">
                         <div class="col-md-12 mt-2">
                             <div class="form-floating mb-3">
@@ -16,7 +17,7 @@
                                 <span class="text-danger error error-value-account"></span>
                             </div>
                             <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="checkbox" role="switch" id="update-input-status-operation" checked>
+                                <input class="form-check-input" type="checkbox" role="switch" id="update-input-status-operation">
                                 <label class="form-check-label" for="update-input-status-operation">Recebido</label>
                                 <span class="text-danger error error-status-operation"></span>
                             </div>
@@ -26,23 +27,23 @@
                                 <span class="text-danger error error-date-operation"></span>
                             </div>
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="update-input-description-operation" placeholder="Descrição" maxlength="30">
+                                <input type="text" class="form-control" id="update-input-description-operation" value="OK" placeholder="Descrição" maxlength="30">
                                 <label for="update-input-description-operation">Descrição: <span class="text-danger">*</span></label>
                                 <span class="text-danger error error-description-operation"></span>
                             </div>
                             <div class="form-floating mb-3">
                                 <select class="form-select" id="update-input-category-operation" aria-label="Floating">
-                                    <option value="1">Salário</option>
-                                    <option value="2">Investimento</option>
-                                    <option value="3">Presente</option>
+                                    <option value="Salário">Salário</option>
+                                    <option value="Investimento">Investimento</option>
+                                    <option value="Presente">Presente</option>
                                 </select>
                                 <label for="update-input-category-operation">Categoria: <span class="text-danger">*</span></label>
                             </div>
                             <div class="form-floating mb-3">
                                 <select class="form-select" id="update-input-account-operation" aria-label="Floating">
-                                    <option value="1">Nubank</option>
-                                    <option value="2">Inter</option>
-                                    <option value="3">Bradesco</option>
+                                    <option value="Nubank">Nubank</option>
+                                    <option value="Inter">Inter</option>
+                                    <option value="Bradesco">Bradesco</option>
                                 </select>
                                 <label for="update-input-account-operation">Conta: <span class="text-danger">*</span></label>
                             </div>
