982a6060e2cd046819bf3611c2dea4e264909db2
Felipe <flipeoliveira007@gmail.com>
2023-07-07 00:09:20 -0300

    MOD: alterando características do modal

--- project/frontend/modal/modal-delete.php
+++ project/frontend/modal/modal-delete.php
@@ -1,9 +1,8 @@
-<div class="modal fade" id="modal-delete" tabindex="-1" aria-hidden="true">
+<div class="modal fade" id="modal-delete" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header bg-danger text-light">
                 <h5 class="modal-title"><b>EXCLUIR RECEITA</b></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form method="post" id="form-delete">
@@ -14,7 +13,7 @@
                         </div>
                     </div>
                     <div class="modal-footer mt-3" id="footer-modal-delete">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CANCELAR</button>
+                        <button type="button" class="btn btn-secondary" id="button-cancel-delete" data-bs-dismiss="modal">CANCELAR</button>
                         <button type="button" class="btn btn-success" id="button-confirm-delete">SIM</button>
                     </div>
                 </form>
