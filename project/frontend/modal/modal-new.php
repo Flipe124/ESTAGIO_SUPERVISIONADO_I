7cdd6121cba290301720769270a043bc7511f38c
Felipe <flipeoliveira007@gmail.com>
2023-07-06 21:26:43 -0300

    MOD: classes de span de mensagens de erro

--- project/frontend/modal/modal-new.php
+++ project/frontend/modal/modal-new.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header bg-success text-light">
-                <h5 class="modal-title"><b>NOVA</b></h5>
+                <h5 class="modal-title"><b>NOVA RECEITA</b></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -13,22 +13,22 @@
                             <div class="form-floating mb-3">
                                 <input type="text" class="form-control" id="create-input-value-operation" placeholder="Valor" oninput="formatValue(this)" maxlength="20">
                                 <label for="create-input-value-operation">Valor: <span class="text-danger">*</span></label>
-                                <span class="text-danger error error-value-account"></span>
+                                <span class="text-danger error error-msg-value-operation"></span>
                             </div>
                             <div class="form-check form-switch form-check-reverse mb-3">
-                                <label class="form-check-label" for="create-input-status-operation">Recebido</label>
+                                <label class="form-check-label" for="create-input-status-operation" value="OK">Recebido</label>
                                 <input class="form-check-input" type="checkbox" role="switch" id="create-input-status-operation" checked>
-                                <span class="text-danger error error-status-operation"></span>
+                                <span class="text-danger error error-msg-status-operation"></span>
                             </div>
                             <div class="form-floating mb-3">
                                 <input type="date" class="form-control" id="create-input-date-operation" placeholder="Data" maxlength="4">
                                 <label for="create-input-date-operation">Data: <span class="text-danger">*</span></label>
-                                <span class="text-danger error error-date-operation"></span>
+                                <span class="text-danger error error-msg-date-operation"></span>
                             </div>
                             <div class="form-floating mb-3">
                                 <input type="text" class="form-control" id="create-input-description-operation" placeholder="Descrição" maxlength="30">
                                 <label for="create-input-description-operation">Descrição: <span class="text-danger">*</span></label>
-                                <span class="text-danger error error-description-operation"></span>
+                                <span class="text-danger error error-msg-description-operation"></span>
                             </div>
                             <div class="form-floating mb-3">
                                 <select class="form-select" id="create-input-category-operation" aria-label="Floating">
@@ -37,6 +37,7 @@
                                     <option value="Presente">Presente</option>
                                 </select>
                                 <label for="create-input-category-operation">Categoria: <span class="text-danger">*</span></label>
+                                <span class="text-danger error error-msg-category-operation"></span>
                             </div>
                             <div class="form-floating mb-3">
                                 <select class="form-select" id="create-input-account-operation" aria-label="Floating">
@@ -45,6 +46,7 @@
                                     <option value="3">Bradesco</option>
                                 </select>
                                 <label for="create-input-account-operation">Conta: <span class="text-danger">*</span></label>
+                                <span class="text-danger error error-msg-account-operation"></span>
                             </div>
                         </div>
                         <div class="col-md-12 mt-2">
