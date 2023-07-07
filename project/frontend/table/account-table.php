ba97461d84e77bbcc002b237e0582dcb9650d30f
Felipe <flipeoliveira007@gmail.com>
2023-07-07 19:08:18 -0300

    REMOVE: código desnecessário

--- project/frontend/table/account-table.php
+++ project/frontend/table/account-table.php
@@ -16,22 +16,6 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="result-table-account text-center">
-                <td>Nubank</td>
-                <td>R$ 4.500,00</td>
-                <td class="text-center">
-                    <button class="btn btn-danger button-delete-account" type="button" data-id="1" data-name-account="Nubank" data-balance-account="4500.00"><i class="fa-solid fa-trash"></i></button>
-                    <button class="btn btn-primary button-update-account" type="button" data-id="1" data-name-account="Nubank" data-balance-account="4500.00"><i class="fa-solid fa-pen"></i></button>
-                </td>
-            </tr>
-            <tr class="result-table-account text-center">
-                <td>Inter</td>
-                <td>R$ 2.300,00</td>
-                <td class="text-center">
-                    <button class="btn btn-danger button-delete-account" type="button" data-id="2" data-name-account="Inter" data-balance-account="2300.00"><i class="fa-solid fa-trash"></i></button>
-                    <button class="btn btn-primary button-update-account" type="button" data-id="2" data-name-account="Inter" data-balance-account="2300.00"><i class="fa-solid fa-pen"></i></button>
-                </td>
-            </tr>
         </tbody>
     </table>
 </div
