d12ddbdeb08d5a767456543d2fe02937db8bfe38
Felipe <flipeoliveira007@gmail.com>
2023-07-06 23:14:31 -0300

    ADD: modal de mensagens de erro

---
+++ project/frontend/modal/modal-message.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="modal-message" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header text-light">
+                <h5><b class="modal-title text-light"></b></h5>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12 message"></div>
+                </div>
+                <div class="modal-footer mt-3" id="footer-modal-error">
+                    <button type="button" id="btn-ok" class="btn btn-success">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
