38092ce9c2b006912211ebbc21063a1a01659bc0
Felipe <flipeoliveira007@gmail.com>
2023-07-07 15:00:51 -0300

    ADD: fun

---
+++ project/frontend/js/transaction/script.js
@@ -0,0 +1,7 @@
+$('#box-dashboard-revenue').on('click', function () {
+    location.replace('./revenue.php');
+});
+
+$('#box-dashboard-expense').on('click', function () {
+    location.replace('./expense.php');
+});
