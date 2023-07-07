002dd61fb4379972df6e188cf43b39d4db70c72f
Felipe <flipeoliveira007@gmail.com>
2023-07-07 14:59:50 -0300

    ADD: importação modal message

--- project/frontend/register/index.php
+++ project/frontend/register/index.php
@@ -75,6 +75,7 @@
                 </form>
             </div>
         </div>
+        <?php require_once("../modal/modal-message.php"); ?>
     </main>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
