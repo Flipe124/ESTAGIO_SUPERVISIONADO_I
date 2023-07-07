a6349d82fea08d8a8d49612a1d877055f9ac9367
Felipe <flipeoliveira007@gmail.com>
2023-07-07 18:06:16 -0300

    ADD: tabela de transações

---
+++ project/frontend/table/transaction-table.php
@@ -0,0 +1,19 @@
+<div class="transaction-table results">
+    <div class="result filter-preset-1" data-id="1" data-category="um" data-value="12300.30">
+        <span class="icon-category">
+            <i class="fab fa-sellcast text-success"></i>
+        </span>
+        <span class="description">
+            <span class="category">
+                <b>Salário</b>
+            </span>
+            <div class="data text-secondary">
+                QuantumTech | Nubank
+            </div>
+            <span class="font-size-14 text-secondary">08/07/2023</span>
+        </span>
+        <div class="value text-success">
+            <b class="text-value">R$ 12.300,30</b> <span class="status mb-1 ms-1"><i class="fas fa-check-circle text-danger"></i></span>
+        </div>
+    </div>
+</div>
