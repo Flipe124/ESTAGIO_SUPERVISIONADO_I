2c6d2cfb3b7256b588247fdef0684b9317550627
Felipe <flipeoliveira007@gmail.com>
2023-07-08 20:43:32 -0300

    REMOVE: código obsoleto

--- project/frontend/includes/header.php
+++ project/frontend/includes/header.php
@@ -10,59 +10,5 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet" />
     <link href="../framework/sidebar/css/styles.css" rel="stylesheet" />
-
-    <link href="../framework/sidebar/css/styles.css" rel="stylesheet" />
     <link href="../css/sidebar/style.css" rel="stylesheet" /> 
     <link href="../css/standard/style.css" rel="stylesheet" />
-
-    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="../includes/components/framework/bootstrap/style.css" rel="stylesheet">
-    <link href="../includes/css/style.css" rel="stylesheet" /> -->
-
-<!-- <!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <link rel="stylesheet" href="../css/style.css">
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-
-
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
-
-  <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&display=swap" rel="stylesheet">
-
-  <link rel="stylesheet" href="../framework/sidebar/fonts/icomoon/style.css">
-
-  <link rel="stylesheet" href="../framework/sidebar/css/owl.carousel.min.css">
-
-  <link rel="stylesheet" href="../framework/sidebar/css/bootstrap.min.css">
-
-  <link rel="stylesheet" href="../framework/sidebar/css/style.css">
-
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
-
-
-  <title>OpenFinance</title>
-</head>
-
-<body>
-
-  <?php
-  // require("../../backend/php/config/connection.php");
-
-  // $connection = new Database();
-
-  // include_once("../includes/function.php");
-
-  // include_once("../includes/request.php");
-  ?> -->
