21c804c636bf0d5f01c51366f599667040bfa6f8
rhuan-pk <rhuan.pksf@gmail.com>
2023-07-08 03:59:59 -0300

    feat: add método get para os endpoints account e category.

---
+++ project/backend/internal/core/account/get.go
@@ -0,0 +1,58 @@
+package account
+
+import (
+	"errors"
+	"net/http"
+
+	"backend/internal/infra/db"
+	"backend/internal/models"
+	"backend/pkg/helpers/structure"
+	"backend/pkg/utils/api"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+// Swagger:
+//
+//	@Summary		GET
+//	@Description	Get a single account from ID.
+//	@Tags			account
+//	@Produce		json
+//	@Param			TOKEN	header		string	true	"Bearer token."
+//	@Param			account	path		int		true	"Account ID."
+//	@Success		200		{object}	models.AccountList
+//	@Failure		404		{object}	models.HTTP
+//	@Failure		500		{object}	models.HTTP
+//	@Router			/account/{account} [get]
+func get(ctx *gin.Context) {
+
+	var account *models.Account
+
+	accountList := &models.AccountList{}
+	ID := ctx.Param("account")
+
+	if err := db.Tx.First(&account, &ID).Error; err != nil {
+
+		code := http.StatusInternalServerError
+		message := http.StatusText(http.StatusInternalServerError)
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			code = http.StatusNotFound
+			message = "account not found"
+		}
+
+		api.LogReturn(
+			ctx,
+			code,
+			message,
+			err.Error(),
+		)
+		return
+
+	}
+	structure.Assign(account, accountList)
+
+	ctx.JSON(http.StatusOK, accountList)
+
+}
---
+++ project/backend/internal/core/category/get.go
@@ -0,0 +1,58 @@
+package category
+
+import (
+	"errors"
+	"net/http"
+
+	"backend/internal/infra/db"
+	"backend/internal/models"
+	"backend/pkg/helpers/structure"
+	"backend/pkg/utils/api"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+// Swagger:
+//
+//	@Summary		GET
+//	@Description	Get a single category from ID.
+//	@Tags			category
+//	@Produce		json
+//	@Param			TOKEN	header		string	true	"Bearer token."
+//	@Param			category	path		int		true	"Category ID."
+//	@Success		200		{object}	models.CategoryList
+//	@Failure		404		{object}	models.HTTP
+//	@Failure		500		{object}	models.HTTP
+//	@Router			/category/{category} [get]
+func get(ctx *gin.Context) {
+
+	var category *models.Category
+
+	categoryList := &models.CategoryList{}
+	ID := ctx.Param("category")
+
+	if err := db.Tx.First(&category, &ID).Error; err != nil {
+
+		code := http.StatusInternalServerError
+		message := http.StatusText(http.StatusInternalServerError)
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			code = http.StatusNotFound
+			message = "category not found"
+		}
+
+		api.LogReturn(
+			ctx,
+			code,
+			message,
+			err.Error(),
+		)
+		return
+
+	}
+	structure.Assign(category, categoryList)
+
+	ctx.JSON(http.StatusOK, categoryList)
+
+}
