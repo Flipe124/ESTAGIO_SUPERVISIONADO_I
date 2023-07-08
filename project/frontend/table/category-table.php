15bee77ebf459e3000409dd7488f7542a462fdf0
Felipe <flipeoliveira007@gmail.com>
2023-07-08 01:03:25 -0300

    ADD: tabela de categoria

---
+++ project/frontend/table/category-table.php
@@ -0,0 +1,21 @@
+<div id="div-table">
+    <table class="table table-striped table-borderless" id="table-category">
+        <thead>
+            <tr>
+                <th class="order-by text-start ps-3" onclick="ordenarTabela(0)">
+                    Nome
+                    <i class="fas fa-sort ml-1"></i>
+                </th>
+                <!-- <th class="order-by text-center" onclick="ordenarTabela(1)">
+                    Saldo
+                    <i class="fas fa-sort ml-1"></i>
+                </th> -->
+                <th class="order-by text-center">
+                    Ações
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
