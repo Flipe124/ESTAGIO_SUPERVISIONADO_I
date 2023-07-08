edea3e26bcb397c56d08c8514ed6b7f48ef8974a
Felipe <flipeoliveira007@gmail.com>
2023-07-08 03:24:48 -0300

    ADD: classes ao cabeçalho da tabela

--- project/frontend/table/account-table.php
+++ project/frontend/table/account-table.php
@@ -2,15 +2,15 @@
     <table class="table table-stripet table-borderless" id="table-account">
         <thead>
             <tr>
-                <th class="order-by text-center" onclick="ordenarTabela(0)">
+                <th class="order-by text-start ps-3" onclick="ordenarTabela(0)">
                     Conta
                     <i class="fas fa-sort ml-1"></i>
                 </th>
-                <th class="order-by text-center" onclick="ordenarTabela(1)">
+                <th class="order-by text-end" onclick="ordenarTabela(1)">
                     Saldo
                     <i class="fas fa-sort ml-1"></i>
                 </th>
-                <th class="order-by text-center">
+                <th class="order-by text-center" style="min-width: 80px;">
                     Ações
                 </th>
             </tr>
