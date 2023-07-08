5198aebf686327151e1bfb9b38ad20dddf238878
Felipe <flipeoliveira007@gmail.com>
2023-07-08 16:17:17 -0300

    MOD: estruturação da página

--- project/frontend/page/transaction.php
+++ project/frontend/page/transaction.php
@@ -11,34 +11,12 @@
                 <div class="col-md">
                     <div class="fs-3"><b>Transações</b></div>
                 </div>
-            </div>
-            <div class="col-md-12 text-center text-light">
-                <div class="box-dashboard mt-1 bg-success filter-preset-1" id="box-dashboard-revenue">
-                    <div class="pt-2 ps-2 text-start">
-                        <i class="fa-solid fa-building-columns"></i> Receitas
-                    </div>
-                    <div class="pt-2 ps-2 fs-6 text-start">
-                        <b>R$ 2.300,50</b>
-                    </div>
-                </div>
-                <div class="box-dashboard mt-1 bg-danger filter-preset-1" id="box-dashboard-expense">
-                    <div class="pt-2 ps-2 text-start">
-                        <i class="fa-solid fa-building-columns"></i> Despesas
-                    </div>
-                    <div class="pt-2 ps-2 fs-6 text-start">
-                        <b>R$ 2.300,50</b>
-                    </div>
+                <div class="col-md d-flex justify-content-end">
+                    <button class="btn btn-success btn-sm mt-1" id="button-new-transaction" type="button">
+                        <i class="fa-solid fa-plus"></i> <b>NOVA</b>
+                    </button>
                 </div>
             </div>
-            <!-- <div class="col-md-12 mt-2">
-                <div class="mt-2 text-center">
-                    <div class="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" class="btn btn-success"><i class="fas fa-arrow-left"></i></button>
-                        <span class="date btn btn-success">Agosto</span>
-                        <button type="button" class="btn btn-success"><i class="fas fa-arrow-right"></i></button>
-                    </div>
-                </div>
-            </div> -->
             <div class="col-md-12">
                 <?php include_once("../table/transaction-table.php") ?>
             </div>
