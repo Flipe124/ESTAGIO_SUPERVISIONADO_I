aac8aab0180185b3ed10429abc45fdedb4b9e29b
Felipe <flipeoliveira007@gmail.com>
2023-07-08 09:27:14 -0300

    ADD

--- project/frontend/js/expense/script.js
+++ project/frontend/js/expense/script.js
@@ -39,9 +39,9 @@ $("#btn-save-new-expense").on("click", function () {
 
 //------------------ Funções ------------------
 
-$("#btn-update-expense").on("click", function (){
+$("#btn-update-expense").on("click", function () {
     let el = document.querySelector(".btn-update-expense");
-    
+
     let dataId = el.getAttribute("data-id");
 
     console.log(dataId)
@@ -57,3 +57,58 @@ $(document).ready(function () {
     $('#select-category').select2();
     $('#select-category-update').select2();
 });
+
+
+
+
+// request 
+
+function requestListExpense() {
+    var accessToken = sessionStorage.getItem('accessToken');
+    var objeto = JSON.parse(accessToken);
+    token = objeto.token;
+
+    console.log(token); // Remover na versão final
+
+    var connect_success = true;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'http://localhost:9999/api/v0/finance/'); // ALTERAR
+
+    xhr.setRequestHeader('Token', `Bearer ${token}`);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            console.log("Status OK");
+            var resposta = JSON.parse(xhr.responseText);
+
+            for (var i = 0; i < resposta.length; i++) {
+                console.log("RESPOSTA -> " + resposta);
+
+                if(resposta[i].type_code == "1") {// 1 saida
+                    console.log("IMPRIMI")
+                    generateTableOperation(resposta[i].account_id, resposta[i].category_id, resposta[i].status_code, resposta[i].type_code, resposta[i].id, resposta[i].datetime, resposta[i].description, resposta[i].value)
+                }
+
+            }
+
+        } else if (xhr.status === 204) {
+            console.log("Sem receitas registradas!");
+
+        } else {
+            connect_success = false;
+
+            var objMessage = JSON.parse(xhr.responseText);
+
+            var code = objMessage.code;
+            var msg = objMessage.error;
+
+            showModalMessage("bg-danger", "ERRO", msg, code);
+
+            return connect_success
+        }
+    };
+
+    xhr.send();
+};
