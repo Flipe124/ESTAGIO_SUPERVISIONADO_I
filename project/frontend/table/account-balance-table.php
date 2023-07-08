5be056e8299f209073df35c8c8c7a76bc96d4dc2
Felipe <flipeoliveira007@gmail.com>
2023-07-08 20:40:36 -0300

    REMOVE: elementos desnecessários

--- project/frontend/table/account-balance-table.php
+++ project/frontend/table/account-balance-table.php
@@ -2,41 +2,15 @@
     <table class="table table-striped table-borderless" id="table-account-balance">
         <thead>
             <tr>
-                <th class="order-by text-start ps-5" onclick="ordenarTabela(0)">
+                <th class="text-start ps-5">
                     Conta
-                    <i class="fas fa-sort ml-1"></i>
                 </th>
-                <!-- <th class="order-by text-center" onclick="ordenarTabela(1)">
-                    Balanço
-                    <i class="fas fa-sort ml-1"></i>
-                </th> -->
-                <th class="order-by text-end pe-3" onclick="ordenarTabela(1)">
+                <th class="text-end pe-4">
                     Saldo
-                    <i class="fas fa-sort ml-1"></i>
                 </th>
             </tr>
         </thead>
-        <tbody>
-            <!-- <tr class="result-table-account text-center">
-                <td>Nubank</td>
-                <td class="text-success">R$ 100,00</td>
-                <td>R$ 4.500,00</td>
-            </tr>
-            <tr class="result-table-account text-center">
-                <td>Inter</td>
-                <td class="text-success">R$ 50,00</td>
-                <td>R$ 2.300,00</td>
-            </tr>
-            <tr class="result-table-account text-center">
-                <td>Carteira</td>
-                <td class="text-danger">R$ 40,00</td>
-                <td>R$ 2.500,00</td>
-            </tr>
-            <tr class="result-table-account text-center">
-                <td>Carteira</td>
-                <td class="text-danger">R$ 100,00</td>
-                <td>R$ 5.500,00</td>
-            </tr> -->
+        <tbody class="results-account">
         </tbody>
     </table>
 </div>
